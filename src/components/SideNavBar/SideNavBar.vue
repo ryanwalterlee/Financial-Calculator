34e1f01117b4ccf07373e162fdd04d58ac894924
Ryan Walter Lee <88182652+ryanwalterlee@users.noreply.github.com>
2021-12-22 17:12:32 +0800

    made nav bar collapsible

--- src/components/SideNavBar/SideNavBar.vue
+++ src/components/SideNavBar/SideNavBar.vue
@@ -1,26 +1,70 @@
 <template>
-  <div class="side-nav-bar">
-    <router-link class="link" to="/" @click.native="clearAllState()">Home</router-link> <!-- @click does not trigger -->
-    <router-link class="link" to="/predictions">Time Series Model</router-link>
+  <div class="container" :class="openHeight">
+    <link rel="stylesheet" 
+        href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
+
+    <div class="button-container">
+      <button class="toggle" @click="() => this.open = !this.open">
+        <i :class="buttonDirection" style="color:white; font-size:200%" ></i>
+      </button>
+    </div>
+
+    <div class="links" v-if="open">
+      <router-link class="link" to="/" @click.native="clearAllState()">Home</router-link> <!-- @click does not trigger -->
+      <router-link class="link" to="/predictions">Time Series Model</router-link>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "SideNavBar",
+  data: function() {
+    return {
+      open: false,
+    }
+  },
+  computed: {
+    buttonDirection: function() {
+      if (this.open) {return "fas fa-angle-left"}
+      else {return "fas fa-angle-right"}
+    },
+    openHeight: function() {
+      if (this.open) {return "open"}
+      else {return ""}
+    }
+  },
   methods: {
     clearAllState: function() {
       this.$store.commit("calculations/clearAll");
-    }
+    },
   }
 };
 </script>
 
 <style scoped>
-
-.side-nav-bar {
-  height: 100%; /* 100% Full-height */
+.container {
   background-color: black;
+}
+
+/* let nav bar height be full when nav bar is open, else not full height */
+.open {
+  height: 100%;
+}
+
+/* styling the arrow button */
+.toggle {
+  background-color: transparent;
+  border: none;
+}
+
+/* positions arrow to the right when it is open */
+.button-container {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.links {
   display:flex;
   flex-direction: column;
   width: 300px;
